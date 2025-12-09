ed5bae9ae203c40997f7dcb581bbca036d679237
Princess Ethel <ethylcess101@gmail.com>
2025-12-09 13:30:21 +0800

    fix: change product card styling when product is sold-out or deleted

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -9,6 +9,7 @@ const { product, isEditable } = defineProps<{ product: Product; isEditable: bool
 const localProduct = ref<Product>(product)
 
 function showProductDetails() {
+  if(localProduct.value.isDeleted) return
   const modal = document.getElementById('modal--product-' + product.id)
   if (modal) {
     const modalInstance = new Modal(modal)
@@ -22,25 +23,63 @@ function updateProduct(newProduct: Product) {
 </script>
 
 <template>
-  <div class="card cursor-pointer shadow-sm" style="width: 20rem" @click="showProductDetails">
+  <div
+    class="product-card card shadow-sm"
+    :class="{ 'cursor-pointer': !localProduct.isDeleted }"
+    style="width: 18rem"
+    @click="showProductDetails"
+  >
     <img
       src="https://plus.unsplash.com/premium_photo-1700740342767-a40f6327c2ce?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Ymx1ZSUyMGl0ZW18ZW58MHx8MHx8fDA%3D"
-      class="card-img-top"
+      class="card-img-top position-relative"
       alt="Product Image"
       style="height: 200px; object-fit: cover"
     />
     <div class="card-body">
       <h5 class="card-title">{{ localProduct.name }}</h5>
-      <p class="card-text text-truncate text-muted">{{ localProduct.description || 'No description provided.' }}</p>
-      <p class="card-text text-muted mb-0">Quantity: {{ localProduct.quantity }}</p>
+
+      <p class="card-text text-truncate text-muted">
+        {{ localProduct.description || 'No description provided.' }}
+      </p>
+      <p v-if="localProduct.quantity > 0" class="card-text text-muted mb-0">
+        Quantity: {{ localProduct.quantity }}
+      </p>
       <p class="card-text mb-0 fs-5 text-primary">{{ formatAmount(localProduct.price) }}</p>
     </div>
+
+    <span v-if="localProduct.quantity < 1" class="sold-out-badge"> Soldout </span>
+    <span v-if="localProduct.isDeleted" class="deleted-badge"> Deleted </span>
   </div>
-  <ViewProduct :product="localProduct" :isEditable="isEditable" @product="updateProduct" />
+  <ViewProduct :product="localProduct" :editable="isEditable" @product="updateProduct" />
 </template>
 
 <style scoped>
+.product-card:has(.deleted-badge) .card-img-top,
+.product-card:has(.sold-out-badge) .card-img-top {
+  opacity: 0.6;
+}
+
 .card:hover {
   background-color: var(--bs-light);
+}
+
+.deleted-badge, .sold-out-badge {
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  z-index: 10;
+}
+
+.deleted-badge {
+  background-color: var(--bs-danger);
+  color: white;
+}
+
+.sold-out-badge {
+  background-color: var(--bs-yellow);
+  color: white;
 }
 </style>
